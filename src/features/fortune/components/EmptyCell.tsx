013f2300e58bbecdbb0f67d7e662ef46d01ac46d
MJ <ckx12@Naver.com>
2025-08-22 10:48:23 +0900

    Feat: 데이터 없을 때 렌더링 컴포넌트 구현

---
+++ src/features/fortune/components/EmptyCell.tsx
@@ -0,0 +1,9 @@
+const EmptyCell = () => {
+  return (
+    <div className="flex flex-col items-center justify-center border-r-[0.5px] border-b bg-white p-1">
+      <div className="text-[3cqi] font-semibold">(없음)</div>
+    </div>
+  );
+};
+
+export default EmptyCell;
